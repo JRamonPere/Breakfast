7c3f7cd9683455a41a2a00befd629ce7e62b87c0
JRamonP <joseramonpereira@hotmail.com>
2023-05-04 09:23:15 -0300

    Estrutura header & Tela inicial

---
+++ script.js
@@ -0,0 +1,6 @@
+function abrir_menu() {
+    document.getElementById("menu_oculto").style.width="220px";
+}
+function menu_fechar() {
+    document.getElementById("menu_oculto").style.width="0px";
+}
