5758b977d19d8512dbce782fbe8c28ce2e97cd69
JRamonP <joseramonpereira@hotmail.com>
2023-05-04 22:27:52 -0300

    Atualizando tela inicial

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function abrir_menu() {
-    document.getElementById("menu_oculto").style.width="220px";
+    document.getElementById("menu_oculto").style.width="13vw";
 }
 function menu_fechar() {
-    document.getElementById("menu_oculto").style.width="0px";
+    document.getElementById("menu_oculto").style.width="0vw";
 }
