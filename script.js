f9b6befa0ae8177401fb22abe98e702eff6154e3
JRamonP <joseramonpereira@hotmail.com>
2023-05-08 22:23:21 -0300

    Tela de Login e Cadastro

--- script.js
+++
@@ -1,6 +0,0 @@
-function abrir_menu() {
-    document.getElementById("menu_oculto").style.width="13vw";
-}
-function menu_fechar() {
-    document.getElementById("menu_oculto").style.width="0vw";
-}
---
+++ tela_inicial/script.js
@@ -0,0 +1,13 @@
+function abrir_menu() {
+    document.getElementById("menu_oculto").style.width="13vw";
+}
+function menu_fechar() {
+    document.getElementById("menu_oculto").style.width="0vw";
+}
+function mostrar_popup_login() {
+    popup_sim_login.classList.remove("disable");
+}
+function mostrar_popup_cadastro() {
+    popup_sim_cadastro.classList.remove("disable");
+}
+
