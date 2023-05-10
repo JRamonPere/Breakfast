b4a46c60f8fc01e2618d13fa23a6ed6a8cc03c81
JRamonP <joseramonpereira@hotmail.com>
2023-05-10 10:02:30 -0300

    Atualizando função popup Form

--- tela_inicial/script.js
+++ tela_inicial/script.js
@@ -4,10 +4,13 @@ function abrir_menu() {
 function menu_fechar() {
     document.getElementById("menu_oculto").style.width="0vw";
 }
+
 function mostrar_popup_login() {
+    popup_sim_cadastro.classList.add("disable-cadastro");
     popup_sim_login.classList.remove("disable");
 }
 function mostrar_popup_cadastro() {
-    popup_sim_cadastro.classList.remove("disable");
+    popup_sim_login.classList.add("disable");
+    popup_sim_cadastro.classList.remove("disable-cadastro");
 }
 
